a0ae4afb81862a774ec8dd8f7fb346e1dbfdd013
GamerVerse722 <cristian987654321rios@gmail.com>
2025-12-02 00:47:20 -0600

    Added ElectricMotor methods

---
+++ src/CCSharp/CreateAddition/ElectricMotor.cs
@@ -0,0 +1,47 @@
+using System;
+using CCSharp.Attributes;
+using CCSharp.ComputerCraft.Peripherals;
+using Microsoft.CodeAnalysis;
+
+namespace CCSharp.CreateAddition;
+
+[LuaImplicitTypeArgument("electric_motor")]
+public class ElectricMotor : CCPeripheral
+{
+    [LuaMethod("getType")]
+    public String GetType() => default;
+
+    [LuaMethod("setSpeed")]
+    public void SetSpeed(double rpm) {}
+    
+    [LuaMethod("stop")]
+    public String Stop() => default;
+
+    [LuaMethod("getSpeed")]
+    public float GetSpeed() => default;
+
+    [LuaMethod("getStressCapacity")]
+    public int GetStressCapacity() => default;
+
+    [LuaMethod("getEnergyConsumption")]
+    public float GetEnergyConsumption() => default;
+    
+    [LuaMethod("rotate")]
+    public float Rotate(double deg, double rpm) => default;
+    
+    [LuaMethod("rotate")]
+    public float Rotate(double deg) => default;
+    
+    [LuaMethod("translate")]
+    public float Translate(double dist, double rpm) => default;
+    
+    [LuaMethod("translate")]
+    public float Translate(double dist) => default;
+
+    [LuaMethod("getMaxInsert")]
+    public int GetMaxInsert() => default;
+
+    [LuaMethod("getMaxExtract")]
+    public int GetMaxExtract() => default;
+
+}
